e07262469708544ec7444fcb983f3d2db9e29f48
femmyte <sanyaoluadefemi@gmail.com>
2021-06-17 07:40:13 -0700

    printing my first statement with puts function in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
-* main function returning 0
+* main - main function
+* return: 0
 */
 
 int main(void){
-puts("Programming is like building a multilingual puzzle\n");
-return 0;
+puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
