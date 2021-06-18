228c431e60b46d27dc1b2599fb932f84c857e329
femmyte <sanyaoluadefemi@gmail.com>
2021-06-18 04:13:15 -0700

    conditional statement to check if x is postive, zero or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,32 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
-* main - consist of conditional statement
+* main - main block
+* Description: Get a random number and print the number
+* check if it is postive or negative
 * Return: 0
 */
 
 int main(void)
 {
         int n;
+	
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-
         if(n > 0)
-        {
-            printf("%d is positive",n);
+	{
+            printf("%d is positive\n",n);
         }
-        else if( n == 0)
+        else if( n < 0)
         {
-            printf("%d is zero",n);
+            printf("%d is negative\n",n);
         }
         else
         {
-            printf("%d is negative",n);
+            printf("%d is zero\n",n);
         }
+	
         return (0);
 }
