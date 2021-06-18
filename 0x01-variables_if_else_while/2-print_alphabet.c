5aa9a82b2ed7a6be20a00ea7550fdb657cae3565
femmyte <sanyaoluadefemi@gmail.com>
2021-06-18 05:04:46 -0700

    program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - consist of conditional statement
+* Description: Get a random number and print the number
+* Return: 0
+*/
+
+int main(void)
+{
+        int x = 97;
+        while (x < 123)
+        {
+	  putchar(x);
+	  x++;
+        }
+        putchar("\n");
+
+	return (0);
+}
