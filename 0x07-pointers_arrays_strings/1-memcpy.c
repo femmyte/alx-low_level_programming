0b545226253ee47aa51b351139d52b38c30c08a7
femmyte <sanyaoluadefemi@gmail.com>
2021-07-05 08:17:17 -0700

    a function  that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * *memcpy - returns a pointer to dest
+ * @dest: the address of memory to print
+ * @src: address of source character
+ * @n: the size of the memory to print
+ *
+ * Return: dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+        for(int i = 0; i <= n; i++)
+        {
+            *(dest+ i) = *(src + i);
+        }
+        return dest;
+}
