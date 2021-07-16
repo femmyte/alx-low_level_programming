b5b536e7337b74234855f8a63c1dfe2b3f500d1a
femmyte <sanyaoluadefemi@gmail.com>
2021-07-16 02:22:40 -0700

    calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == 0)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
 
 	arr[i] = '\0';
