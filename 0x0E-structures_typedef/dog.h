dab4b227e16a8b7b1cb176e8c96c08a2cf2c9cb8
femmyte <sanyaoluadefemi@gmail.com>
2021-07-19 08:38:15 -0700

    updated the dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: declared three members for dog struct
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+	/* declared three members for dog struct */
+
 #endif /* DOG_H */
