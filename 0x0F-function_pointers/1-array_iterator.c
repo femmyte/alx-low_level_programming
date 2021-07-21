166b9c88aea573c8bc474abfa6a4d6bf966b3d23
femmyte <sanyaoluadefemi@gmail.com>
2021-07-21 02:30:26 -0700

    funtion that print an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - printing a name
+ * @array: variable
+ * @size: size of the array
+ * @action: function pointer
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
