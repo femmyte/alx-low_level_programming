014b1f8243fa28ada6820452101c05cbb268cf70
femmyte <sanyaoluadefemi@gmail.com>
2021-07-23 14:11:02 -0700

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,85 +1,57 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
 
 /**
- * print_int - prints int
- * @list: arguments from print_all
- */
-void print_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_float - prints float
- * @list: arguments from print_all
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * print_char - prints int
- * @list: arguments from print_all
- */
-void print_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-
-/**
- * print_str - prints string
- * @list: arguments from print_all
- */
-void print_str(va_list list)
-{
-	char *s = va_arg(list, char *);
-
-	s == NULL ? printf("(nil)") : printf("%s", s);
-
-}
-
-/**
- * print_all - prints any type
- * @format: arguments to print
+ * print_all - check the code for ALX School students.
+ * @format: variable
+ * Return: Nothing
  */
 
 void print_all(const char * const format, ...)
 {
-va_list list;
-int i = 0, j = 0;
-char *sep = "";
+	va_list ap;
+	char *sval, cval;
+	int i = 0, ival;
+	double dval;
 
-printTypeStruct printType[] = {
-	{ "i", print_int },
-	{ "f", print_float },
-	{ "c", print_char },
-	{ "s", print_str },
-	{NULL, NULL}
-};
-
-
-va_start(list, format);
-
-while (format && format[i])
-{
-	j = 0;
-	while (j < 4)
+	va_start(ap, format);
+	/*p = format;*/
+	while(format && format[i] && i < 3)
 	{
-		if (*printType[j].type == format[i])
+		if(*format != '%')
 		{
-			printf("%s", sep);
-			printType[j].printer(list);
-			sep = ", ";
-			break;
+			putchar(*format);
+			continue;
 		}
-		j++;
+		switch(*format)
+		{
+		case 'i':
+			ival = va_arg(ap, int);
+			printf("%d", ival);
+			break;
+		case 'f':
+			dval = va_arg(ap, double);
+			printf("%f", dval);
+			break;
+		case 's':
+			sval = va_arg(ap, char *);
+			if (sval == NULL)
+				printf("(nil)");
+			while(*sval)
+			{
+				putchar(*sval);
+				sval++;
+			}
+			break;
+		case 'c':
+			cval = va_arg(ap, int);
+			printf("%c", cval);
+		}
+		i++;
+		/*format++;*/
 	}
-	i++;
-}
-
-printf("\n");
-va_end(list);
+	va_end(ap);
+	putchar('\n');
 }
